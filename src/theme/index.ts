facf51610b4b65cc0eac1b79d2b3d5c02ab90396
Edy Espinal <edyespinal@gmail.com>
2022-05-04 10:33:00 +0200

    feat: add theme

---
+++ src/theme/index.ts
@@ -0,0 +1,24 @@
+import { MantineThemeOverride } from '@mantine/core'
+
+export const theme: MantineThemeOverride = {
+  white: '#f4f5f6',
+  black: '#333',
+  lineHeight: 1.65,
+  colors: {
+    blue: ['#3a7adb'],
+  },
+  fontSizes: {
+    xs: 10,
+    sm: 14,
+    md: 17,
+    lg: 20,
+    xl: 24,
+  },
+  spacing: {
+    xs: 10,
+    sm: 16,
+    md: 20,
+    lg: 32,
+    xl: 40,
+  },
+}
